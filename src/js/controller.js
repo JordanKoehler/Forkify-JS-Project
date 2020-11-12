04f2969a319fd077f903662fb6c190b020b73777
Jordan Koehler <digipower66@gmail.com>
2020-11-12 11:27:56 -0500

    Pagination feature complete

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,14 +2,15 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 // import icons from '../img/icons.svg'; //Parcel 1
 import icons from 'url:../img/icons.svg';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-if (module.hot) {
-  module.hot.accept();
-}
+// if (module.hot) {
+//   module.hot.accept();
+// }
 
 const controlRecipes = async function () {
   try {
@@ -39,15 +40,26 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     //3) Render results
+    resultsView.render(model.getSearchResultsPage());
 
-    resultsView.render(model.getSearchResultsPage(1));
+    //4) Pagination
+    paginationView.render(model.state.search);
   } catch (err) {
     console.error(err);
   }
 };
 
+const controlPagination = function (goToPage) {
+  //1) Render NEW results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  //2) NEW Pagination
+  paginationView.render(model.state.search);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 init();
--- src/js/model.js
+++ src/js/model.js
@@ -27,7 +27,7 @@ export const loadRecipe = async function (id) {
       ingredients: recipe.ingredients,
     };
 
-    console.log(recipe);
+    // console.log(recipe);
   } catch (err) {
     console.error(`${err} 💥💥💥`);
     throw err;
@@ -38,7 +38,7 @@ export const loadSearchResults = async function (query) {
   try {
     state.search.query = query;
     const data = await getJSON(`${API_URL}?search=${query}`);
-    console.log(data);
+    // console.log(data);
 
     state.search.results = data.data.recipes.map(rec => {
       return {
